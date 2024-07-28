0fb65510179bb47b3ec71aa9a4fa7350fecbb553
Oğuzhan Bolat <oguzhanbolat0@gmail.com>
2024-07-28 22:49:13 +0300

    Initial Commit

---
+++ GroceryApp/GroceryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryAppApp.swift
+//  GroceryApp
+//
+//  Created by Oğuzhan Bolat on 28.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct GroceryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
